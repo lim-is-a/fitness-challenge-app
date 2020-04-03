449b1208ed40b7eaa2ae849623e0ab5ff10d7cbd
iLimas <ilimasc3@gmail.com>
2020-04-03 15:20:36 -0400

    created models

--- fitness_challenge/models.py
+++ fitness_challenge/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Challenge(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField()
+
+    def __str__(self):
+        return f'{self.image_url}{self.name}'
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField()
+    description = models.CharField(max_length=200)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='exercises')
+
+    def __str__(self):
+        return f'{self.name}{self.image}{self.description}'
