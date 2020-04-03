62db7b82c0c241538bc03d88f516510c173cded4
iLimas <ilimasc3@gmail.com>
2020-04-03 18:40:29 -0400

    removed exercise list from views.py

--- fitness_challenge/views.py
+++ fitness_challenge/views.py
@@ -7,10 +7,6 @@ def challenge_list(request):
     challenges = Challenge.objects.all()
     return render(request, 'fitness_challenge/challenge_list.html', {'challenges': challenges})
 
-def exercise_list(request):
-    exercises = Exercise.objects.all()
-    return render(request, 'fitness_challenge/exercise_list.html', {'exercises': exercises})
-
 def challenge_detail(request, pk):
     challenge = Challenge.objects.get(pk=pk)
     return render(request, 'fitness_challenge/challenge_detail.html', {'challenge': challenge})
