2b4ca325a96b7ded8ef8dbbd5bc2c0335deb6911
iLimas <ilimasc3@gmail.com>
2020-04-03 16:33:59 -0400

    edited models, ran migrations

--- fitness_challenge/models.py
+++ fitness_challenge/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 class Challenge(models.Model):
     name = models.CharField(max_length=100)
-    image_url = models.TextField()
+    image_url = models.CharField(max_length=500, null = True)
 
     def __str__(self):
-        return f'{self.image_url}{self.name}'
+        return self.name
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
-    image_url = models.TextField()
+    image_url = models.CharField(max_length=500)
     description = models.CharField(max_length=200)
-    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='exercises')
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='exerciseList')
 
     def __str__(self):
-        return f'{self.name}{self.image}{self.description}'
+        return f'{self.name}{self.image_url}{self.description}'
