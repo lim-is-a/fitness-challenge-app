7d74cfd5314e8d013f45d9ce794c0d990f948acc
iLimas <ilimasc3@gmail.com>
2020-04-06 11:41:23 -0400

    added result to admin, and added resultslink to nav

--- fitness_challenge/admin.py
+++ fitness_challenge/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Challenge, Exercise
+from .models import Challenge, Exercise, Result
 
 admin.site.register(Challenge)
 admin.site.register(Exercise)
+admin.site.register(Result)
