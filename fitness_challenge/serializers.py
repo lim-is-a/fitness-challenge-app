48fa13ea9bf9ea9bdb09d0998769aaa753dd7423
iLimas <ilimasc3@gmail.com>
2020-04-06 19:01:39 -0400

    rearranged serializers

--- fitness_challenge/serializers.py
+++ fitness_challenge/serializers.py
@@ -1,14 +1,6 @@
 from rest_framework import serializers
 from .models import Challenge, Exercise, Result
 
-class ChallengeSerializer(serializers.ModelSerializer):
-    exerciseList = ExerciseSerializer(
-        many=True,
-        read_only=True
-    )
-    class Meta:
-        model = Challenge
-        fields = ('id', 'name', 'image_url', 'exerciseList')
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +11,9 @@ class ResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = Result
         fields = ('id', 'challenge_name', 'name', 'date', 'time', 'avg_hr','max_hr', 'cals_burned', 'note')
+
+class ChallengeSerializer(serializers.ModelSerializer):
+    exerciseList = ExerciseSerializer(many=True, read_only=True)
+    class Meta:
+        model = Challenge
+        fields = ('id', 'name', 'image_url', 'exerciseList')
