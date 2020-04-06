c040058ac24f2e130422a6bdb53b3b45c443c8d1
iLimas <ilimasc3@gmail.com>
2020-04-05 23:02:57 -0400

    added exercise serializer

--- fitness_challenge/serializers.py
+++ fitness_challenge/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
-from .models import Challenge
+from .models import Challenge, Exercise
 
 class ChallengeSerializer(serializers.HyperlinkedModelSerializer):
     exerciseList = serializers.HyperlinkedRelatedField(
         many=True,
         read_only=True
     )
-class Meta:
-    model = Challenge
-    fields = ('id', 'name', 'image_url', 'exerciseList')
+    class Meta:
+        model = Challenge
+        fields = ('id', 'name', 'image_url', 'exerciseList')
+
+class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
+    challenge = serializers.HyperlinkedRelatedField(
+        view_name = challenge_detail, 
+        many = False, 
+        read_only = True, 
+        queryset = Challenge.objects.all(),
+    )
+    class Meta:
+        model = Exercise
+        fields = ('id', 'name', 'image_url', 'description', 'challenge', 'challenge')
